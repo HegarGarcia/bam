cacf27954e708cf1ead56f5752c033bf6c020cbe
Hegar Garcia <hegargarcia@gmail.com>
2018-07-07 02:11:51 -0500

    Importing Angular Material Modules

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 // Angular Material Modules
-import { MatToolbarModule, MatSidenavModule } from '@angular/material';
+import {
+  MatToolbarModule,
+  MatSidenavModule,
+  MatListModule
+} from '@angular/material';
 
 import { NavComponent } from './nav/nav.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
-  imports: [CommonModule, MatToolbarModule, MatSidenavModule],
+  imports: [CommonModule, MatToolbarModule, MatSidenavModule, MatListModule],
   exports: [NavComponent],
   declarations: [NavComponent, SideNavComponent, ToolbarComponent]
 })
