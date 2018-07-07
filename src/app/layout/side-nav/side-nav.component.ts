59b9ac1c02008e179bc3d755e4edb453eae7caaa
Hegar Garcia <hegargarcia@gmail.com>
2018-07-07 02:12:13 -0500

    Adding Side Nav

--- src/app/layout/side-nav/side-nav.component.ts
+++ src/app/layout/side-nav/side-nav.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-nav.component.css']
 })
 export class SideNavComponent implements OnInit {
+  public categories: [Category];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.categories = [{ name: 'Naranjas' }];
   }
 
+  ngOnInit() {}
+}
+
+interface Category {
+  name: string;
 }
