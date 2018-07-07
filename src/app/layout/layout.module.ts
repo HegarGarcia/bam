bdf0e6fa95c513b15dbb0586bf39fad951d20e93
Hegar Garcia <hegargarcia@gmail.com>
2018-07-07 01:19:43 -0500

    Adding Angular Material Modules

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Angular Material Modules
+import { MatToolbarModule, MatSidenavModule } from '@angular/material';
+
 import { NavComponent } from './nav/nav.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MatToolbarModule, MatSidenavModule],
   exports: [NavComponent],
   declarations: [NavComponent, SideNavComponent, ToolbarComponent]
 })
