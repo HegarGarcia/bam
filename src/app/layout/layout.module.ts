10a05d51d5a6bb67a5d64fee6f38d2ed7529e2d9
theoliver264 <31642690+theoliver264@users.noreply.github.com>
2018-07-07 02:07:02 -0500

    Add Toolbar

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 // Angular Material Modules
-import { MatToolbarModule, MatSidenavModule } from '@angular/material';
+import {MatSidenavModule } from '@angular/material';
 
+import {MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { NavComponent } from './nav/nav.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [CommonModule, MatToolbarModule, MatSidenavModule],
+  imports: [CommonModule, MatToolbarModule, MatSidenavModule, MatButtonModule, MatIconModule],
   exports: [NavComponent],
   declarations: [NavComponent, SideNavComponent, ToolbarComponent]
 })
