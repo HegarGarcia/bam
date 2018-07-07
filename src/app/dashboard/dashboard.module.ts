e3a98c652d35baf6dd89cd53654de9ccab50edb1
theoliver264 <31642690+theoliver264@users.noreply.github.com>
2018-07-07 06:42:56 -0500

    Placeholder

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingComponent } from './landing/landing.component';
 
+import {MatCardModule} from '@angular/material/card'
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule
   ],
   exports: [LandingComponent],
   declarations: [LandingComponent]
