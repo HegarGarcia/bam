614a7e2fd2d9a19d760d7d593648c6a880ce2109
Hegar Garcia <hegargarcia@gmail.com>
2018-07-07 01:01:51 -0500

    Adding Toolbar and Side Nav to Module

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NavComponent } from './nav/nav.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   imports: [CommonModule],
   exports: [NavComponent],
-  declarations: [NavComponent]
+  declarations: [NavComponent, SideNavComponent, ToolbarComponent]
 })
 export class LayoutModule {}
