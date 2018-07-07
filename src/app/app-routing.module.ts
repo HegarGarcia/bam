0f61217a62e5f79042255c3b542935e470baac52
Hegar Garcia <hegargarcia@gmail.com>
2018-07-07 12:24:18 -0500

    Adding Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LandingComponent } from './dashboard/landing/landing.component';
+import { LoginComponent } from './login/login/login.component';
+
+const routes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
