295648f1f481db27e68406b3b8927930dc370ec7
Hegar Garcia <hegargarcia@gmail.com>
2018-07-07 00:53:23 -0500

    Creating the Auth Service

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
 
+import { auth } from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Router } from '@angular/router';
+
+import { Observable, of } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  public user: Observable<firebase.User>;
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth, private router: Router) {
+    this.user = this.afAuth.authState;
+  }
+
+  googleLogin() {
+    const provider = new auth.GoogleAuthProvider();
+    this.afAuth.auth
+      .signInWithPopup(provider)
+      .then(credentials => (this.user = of(credentials.user)))
+      .catch(err => console.error(err));
+  }
+
+  signOut() {
+    this.afAuth.auth.signOut().then(() => (this.user = null));
+  }
 }
