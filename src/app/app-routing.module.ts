1352ace98f1c1d6cc490e3f1ce778f9b2626ae44
Hegar Garcia <hegargarcia@gmail.com>
2018-07-08 09:23:51 -0500

    Bug fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LandingComponent } from './dashboard/landing/landing.component';
 import { LoginComponent } from './login/login/login.component';
-import { ProductDetailComponent } from "./products/product-detail/product-detail.component";
+import { ProdDescComponent } from './product-detail/prod-desc/prod-desc.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'product/:id', component: ProductDetailComponent, pathMatch: 'full' }
+  { path: 'product/:id', component: ProdDescComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
