6ddb37ea95f7c519131766179679ba6a564ed85b
Hegar Garcia <hegargarcia@gmail.com>
2018-07-08 01:03:11 -0500

    Adding Firebase to Env Variables in Production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyACHkj3Y4oo9mVN6asshoGkef81SQvKLEA',
+    authDomain: 'bam-mx.firebaseapp.com',
+    databaseURL: 'https://bam-mx.firebaseio.com',
+    projectId: 'bam-mx',
+    storageBucket: '',
+    messagingSenderId: '409700494601'
+  }
 };
